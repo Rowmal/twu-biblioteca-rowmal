5819ec7a82a57d234cf63f9dd7cccf72e4b77110
rowmal <rowmal@protonmail.com>
2019-04-01 22:43:05 +1100

    [1.4] Add main menu and option selection

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,13 +1,27 @@
 package com.twu.biblioteca;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class BibliotecaApp {
 
-    public static void main(String[] args) {
-        System.out.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
+    public static void main(String[] args) throws IOException {
+        // TODO: Print to console using PrintWriter
         String[] libraryBooks = { "A Game of Thrones", "Nineteen Eighty-Four", "The Metamorphosis" };
         String[] pubYears = { "1996", "1949", "1915" };
         String[] authors = { "George R. R. Martin", "George Orwell", "Franz Kafka" };
+
+        System.out.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
+        System.out.println("[1] List of books");
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str;
+        do {
+            str = br.readLine();
+        } while (!str.equals("1"));
         for (int i = 0; i < 3; i++)
             System.out.printf("%-30s|%-30s|%-4.4s%n", libraryBooks[i], authors[i], pubYears[i]);
+        br.close();
     }
 }
--- test/com/twu/biblioteca/BibliotecaTest.java
+++ test/com/twu/biblioteca/BibliotecaTest.java
@@ -4,8 +4,7 @@ import org.junit.Before;
 import org.junit.After;
 import org.junit.Test;
 
-import java.io.PrintStream;
-import java.io.ByteArrayOutputStream;
+import java.io.*;
 
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.startsWith;
@@ -13,47 +12,73 @@ import static org.hamcrest.CoreMatchers.containsString;
 
 public class BibliotecaTest {
 
+    private InputStream systemIn;
     private PrintStream systemOut;
-    private final ByteArrayOutputStream actualOut = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream output = new ByteArrayOutputStream();
 
     @Before
-    public void mockSystemOut() {
+    public void mockSystemStreams() {
+        systemIn = System.in;
         systemOut = System.out;
-        System.setOut(new PrintStream(actualOut));
+        System.setOut(new PrintStream(output));
     }
 
     @After
     public void revertStreams() {
-        System.out.close();
+        System.setIn(systemIn);
         System.setOut(systemOut);
+        output.reset();
     }
 
     @Test
-    public void welcomeMessageShouldBeDisplayedWhenApplicationStarts() {
-        BibliotecaApp.main(null);
+    public void welcomeMessageShouldDisplayWhenApplicationStarts() throws IOException {
         String expectedWelcome = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!\n";
-        assertThat(actualOut.toString(), startsWith(expectedWelcome));
+        String input = "1\n";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        BibliotecaApp.main(null);
+
+        assertThat(output.toString(), startsWith(expectedWelcome));
     }
 
     @Test
-    public void booksShouldBeListedAfterWelcomeMessage() {
+    public void mainMenuShouldDisplayAfterWelcomeMessage() throws IOException {
+        String expectedMainMenu = "[1] List of books\n";
+        String input = "1\n";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
         BibliotecaApp.main(null);
+
+        assertThat(output.toString(), containsString(expectedMainMenu));
+    }
+
+    @Test
+    public void booksShouldBeListedAfterWelcomeMessage() throws IOException {
         String[] expectedBookList = { "A Game of Thrones", "Nineteen Eighty-Four", "The Metamorphosis" };
-        String actualOutStr = actualOut.toString();
+        String input = "1\n";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        BibliotecaApp.main(null);
+
+        String outputStr = output.toString();
         for (int i = 0; i < 3; i++) {
-            assertThat(actualOutStr, containsString(expectedBookList[i]));
+            assertThat(outputStr, containsString(expectedBookList[i]));
         }
     }
 
     @Test
-    public void authorAndPublicationYearShouldBeListed() {
+    public void authorAndPublicationYearShouldBeListed() throws IOException {
         String[] expectedPubYears = { "1996", "1949", "1915" };
         String[] expectedAuthors = { "George R. R. Martin", "George Orwell", "Franz Kafka" };
+        String input = "1\n";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
         BibliotecaApp.main(null);
-        String actualOutStr = actualOut.toString();
+
+        String outputStr = output.toString();
         for (int i = 0; i < 3; i++) {
-            assertThat(actualOutStr, containsString(expectedPubYears[i]));
-            assertThat(actualOutStr, containsString(expectedAuthors[i]));
+            assertThat(outputStr, containsString(expectedPubYears[i]));
+            assertThat(outputStr, containsString(expectedAuthors[i]));
         }
     }
 }
