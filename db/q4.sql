54700665eede2cb99dedfc43d8be074567c4948d
rowmal <rowmal@protonmail.com>
2019-04-03 14:33:26 +1100

    [SQL] Add solution for exercise 4

---
+++ db/q4.sql
@@ -0,0 +1,19 @@
+--Verify that I've checked out 'The Pragmatic Programmer'
+INSERT INTO book (id, title)
+VALUES (11, 'The Pragmatic Programmer');
+
+INSERT INTO member (id, name)
+VALUES (43, 'Rowan Malamoo');
+
+INSERT INTO checkout_item (member_id, book_id)
+VALUES (43, 11);
+
+SELECT member.name
+FROM member
+WHERE member.id
+IN (
+    SELECT checkout_item.member_id
+    FROM checkout_item, book
+    WHERE checkout_item.book_id = book.id
+    AND book.title = 'The Pragmatic Programmer'
+);
